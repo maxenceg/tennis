dfe5af672637f660fa251a4f5490bc0afed8dd87
Maxence Gilliot <maxencegilliot@gmail.com>
2018-12-08 22:20:19 +0100

    Update App.js to make use of newest components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
-import Club from "./components/Club/Club";
+import Club from "./components/Club";
+import styles from "./App.style";
+import EditionMenu from "./components/EditionMenu";
+import { Provider } from "react-redux";
+import configureStore from "./store/tennisStore";
 
+const store = configureStore();
 class App extends Component {
   render() {
     return (
-      <div>
-        <Club />
-      </div>
+      <Provider store={store}>
+        <div style={styles.appContainer}>
+          <Club />
+          <EditionMenu />
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/App.style.js
@@ -0,0 +1,9 @@
+const styles = {
+  appContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    height: "100vh"
+  }
+};
+
+export default styles;
